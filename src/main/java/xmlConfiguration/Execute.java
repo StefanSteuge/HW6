e1e97a8d10a1c4724bc76ff67434c12d618bf0a6
Stepan Dulka <fotosteuge@gmail.com>
2022-08-09 18:49:12 +0300

    add code to xml branch

--- src/main/java/xmlConfiguration/Execute.java
+++ src/main/java/xmlConfiguration/Execute.java
@@ -6,9 +6,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Execute {
         public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("xmlConfig.xml");
-        Calculator calculator = (Calculator) beanFactory.getBean("calculator");
 
-                System.out.println(calculator.getMinusService().getResultMinus());
-                System.out.println(calculator.getPlusService().getResultPlus());
+        MinusService minusService = beanFactory.getBean("minusService", MinusService.class);
+        PlusService plusService = beanFactory.getBean("plusService", PlusService.class);
+                System.out.println(minusService.getResultMinus());
+                System.out.println(plusService.getResultPlus());
+
+//        Calculator calculator = (Calculator) beanFactory.getBean("calculator");
+//                System.out.println(calculator.getMinusService().getResultMinus());
+//                System.out.println(calculator.getPlusService().getResultPlus());
     }
 }
